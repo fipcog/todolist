a877613788990e9f782fe1290cc4bebdaed41b54
Artur <looppinooppi@gmail.com>
2023-09-12 18:26:06 +0300

    done

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,21 @@
 import React from 'react';
 import './App.css';
+import { TaskKard, TaskType } from './components/TaskKard';
+
+const tasks: Array<TaskType> = [
+    {
+        title: "Task Title",
+        taskCheckboxes: [
+            {id: 1, isChecked: true, name:'HTML&CSS'},
+            {id: 2, isChecked: false, name:'JS'},
+            {id: 3, isChecked: false, name:'React'}
+        ]
+    },
+]
 
 function App() {
     return (
-        <div className="App">
-            <div>
-                <h3>What to learn</h3>
-                <div>
-                    <input/>
-                    <button>+</button>
-                </div>
-                <ul>
-                    <li><input type="checkbox" checked={true}/> <span>HTML&CSS</span></li>
-                    <li><input type="checkbox" checked={true}/> <span>JS</span></li>
-                    <li><input type="checkbox" checked={false}/> <span>React</span></li>
-                </ul>
-                <div>
-                    <button>All</button>
-                    <button>Active</button>
-                    <button>Completed</button>
-                </div>
-            </div>
-        </div>
+        <TaskKard data={tasks}/>
     );
 }
 
