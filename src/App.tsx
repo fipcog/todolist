c96ca5606ebe51e6352317283c6ba268833247a0
Artur <looppinooppi@gmail.com>
2023-10-28 14:41:31 +0300

    states removed

--- src/App.tsx
+++ src/App.tsx
@@ -22,32 +22,10 @@ export const App: React.FC = () => {
     const todolistID1=crypto.randomUUID();
     const todolistID2=crypto.randomUUID();
 
-    // const [todolists, setTodolists] = useState<TodolistType[]>([
-    //     {id:todolistID1, title: 'What to learn', filter: 'all'},
-    //     {id:todolistID2, title: 'What to buy', filter: 'all'},
-    // ])
-
     const [todolists, dispatchTodolists] = useReducer(todolistReducer, [
         {id:todolistID1, title: 'What to learn', filter: 'all'},
         {id:todolistID2, title: 'What to buy', filter: 'all'},
     ])
-
-    // const [tasks, setTasks] = useState<TasksType>({
-    //     [todolistID1]:[
-    //         {id: crypto.randomUUID(), title: "HTML&CSS", isDone: true},
-    //         {id: crypto.randomUUID(), title: "JS", isDone: true},
-    //         {id: crypto.randomUUID(), title: "ReactJS", isDone: false},
-    //         {id: crypto.randomUUID(), title: "Rest API", isDone: false},
-    //         {id: crypto.randomUUID(), title: "GraphQL", isDone: false},
-    //     ],
-    //     [todolistID2]:[
-    //         {id: crypto.randomUUID(), title: "HTML&CSS2", isDone: true},
-    //         {id: crypto.randomUUID(), title: "JS2", isDone: true},
-    //         {id: crypto.randomUUID(), title: "ReactJS2", isDone: false},
-    //         {id: crypto.randomUUID(), title: "Rest API2", isDone: false},
-    //         {id: crypto.randomUUID(), title: "GraphQL2", isDone: false},
-    //     ]
-    // });
 
     const [tasks, dispatchTasks] = useReducer(tasksReducer, {
         [todolistID1]:[
@@ -67,22 +45,18 @@ export const App: React.FC = () => {
     });
 
     const toggleIsChecked = (todolistID: string, taskID: string): void => {
-        // setTasks({...tasks, [todolistID]: [...tasks[todolistID].map(task => task.id === taskID ? {...task, isDone: !task.isDone}: task)]})
         dispatchTasks(toggleIsCheckedAC(todolistID, taskID))
     }
 
     const removeTask = (todolistID: string , taskID: string): void => {
-        // setTasks({...tasks, [todolistID]: [...tasks[todolistID].filter(task => task.id !== taskID)]})
         dispatchTasks(removeTaskAC(todolistID, taskID))
     }
 
     const addTask = (todolistID: string, taskName: string): void => {
-        // setTasks({...tasks, [todolistID]: [{id:crypto.randomUUID(), title: taskName, isDone: false}, ...tasks[todolistID]]})
         dispatchTasks(addTaskAC(todolistID, taskName))
     }
 
     const removeTodolist = (todolistID: string): void => {
-        // setTodolists(todolists.filter(tdList => tdList.id !== todolistID))
         dispatchTodolists(removeTodolistAC(todolistID))
         delete tasks[todolistID]
     }
@@ -92,17 +66,13 @@ export const App: React.FC = () => {
         const newTodo: TodolistType = {id: NewTodolistID, title, filter: 'all'}
         dispatchTodolists(createNewTodolistAC(newTodo))
         dispatchTasks(addNewEmptyTasksList(NewTodolistID))
-        // setTodolists([...todolists, newTodo])
-        // setTasks({...tasks, [todoID]: []})
     }
 
     const changeTaskTitle = (todolistID: string, taskID: string ,taskTitle: string): void => {
-        // setTasks({...tasks, [todolistID]: tasks[todolistID].map(task => task.id === taskID ? {...task, title: taskTitle} : task)})
         dispatchTasks(changeTaskTitleAC(todolistID, taskID, taskTitle))
     }
 
     const changeTodolistTitle = (todolistID: string, todoTitle: string): void => {
-        // setTodolists(todolists.map(todo => todo.id === todolistID ? {...todo, title: todoTitle} : todo))
         dispatchTodolists(changeTodolistTitleAC(todolistID, todoTitle))
     }
 
